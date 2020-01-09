9838dbd00d7581d7cf1bcceb59932299f68ccd9a
Ozge Yurtsever <ozge.ikiz.yurtsever@gmail.com>
2020-01-09 13:31:30 -0800

    Render tabs and contents

--- src/components/sideBar/sidebarContent.jsx
+++ src/components/sideBar/sidebarContent.jsx
@@ -2,11 +2,9 @@ import React, { Component } from "react";
 
 const sidebarContent = props => {
   return (
-    <div>
-      <table>
-        <tbody>{props.children}</tbody>
-      </table>
-    </div>
+    <table>
+      <tbody>{props.children}</tbody>
+    </table>
   );
 };
 
