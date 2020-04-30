d09808ab368fd5e0ddbd10743e2f29266cdf8132
Mete Akdogan <muakdogan@gmail.com>
2020-04-30 14:33:08 -0700

    updates aim to order compatible with xml

--- src/components/aimEditor/Aim.jsx
+++ src/components/aimEditor/Aim.jsx
@@ -13,7 +13,7 @@ class Aim {
       segmentation: this.temp.segmentation,
       equipment: this.temp.equipment,
       user: this.temp.user,
-      person: this.temp.person
+      person: this.temp.person,
     } = imageData);
     this.xmlns = aimConf.xmlns;
     this["xmlns:rdf"] = aimConf["xmlns:rdf"];
@@ -29,7 +29,7 @@ class Aim {
     this.equipment = this._createEquipment(this.temp.equipment);
     this.person = this._createPerson(this.temp.person);
     this.imageAnnotations = {
-      ImageAnnotation: [this._createImageAnnotations(aimType)]
+      ImageAnnotation: [this._createImageAnnotations(aimType)],
     };
     if (updatedAimId === undefined)
       this.uniqueIdentifier = { root: generateUid() };
@@ -106,11 +106,11 @@ class Aim {
       Dimension: [
         Object.assign(
           {},
-          this._createObject("size", size),
           this._createObject("index", index),
+          this._createObject("size", size),
           this._createObject("label", label)
-        )
-      ]
+        ),
+      ],
     };
   };
 
@@ -119,8 +119,11 @@ class Aim {
       dataType: {
         code: "C48870",
         codeSystem: "NCI",
-        "iso:displayName": { "xmlns:iso": "uri:iso.org:21090", value: "Double" }
-      }
+        "iso:displayName": {
+          "xmlns:iso": "uri:iso.org:21090",
+          value: "Double",
+        },
+      },
     };
     return obj;
   };
@@ -147,53 +150,53 @@ class Aim {
     obj["codeSystemName"] = codeSystemName;
     obj["iso:displayName"] = {
       "xmlns:iso": "uri:iso.org:21090",
-      value: displayNameValue
+      value: displayNameValue,
     };
     return obj;
   };
 
-  createLengthCalcEntity = length => {
+  createLengthCalcEntity = (length) => {
     let { unit, value } = length;
     var obj = {};
-    obj["calculationResultCollection"] = {
-      CalculationResult: [this._createCalcResult(unit, "LineLength", value)]
-    };
-    obj["description"] = { value: "Length" };
     const uId = generateUid();
     obj["uniqueIdentifier"] = { root: uId };
     obj["typeCode"] = [this._createTypeCode("G-D7FE", "SRT", "Length")];
+    obj["description"] = { value: "Length" };
+    obj["calculationResultCollection"] = {
+      CalculationResult: [this._createCalcResult(unit, "LineLength", value)],
+    };
     this.imageAnnotations.ImageAnnotation[0].calculationEntityCollection[
       "CalculationEntity"
     ].push(obj);
     return uId;
   };
 
-  createLongAxisCalcEntity = longAxis => {
+  createLongAxisCalcEntity = (longAxis) => {
     let { unit, value } = longAxis;
     var obj = {};
-    obj["calculationResultCollection"] = {
-      CalculationResult: [this._createCalcResult(unit, "LongAxis", value)]
-    };
-    obj["description"] = { value: "LongAxis" };
     const uId = generateUid();
     obj["uniqueIdentifier"] = { root: uId };
     obj["typeCode"] = [this._createTypeCode("G-A185", "SRT", "LongAxis")];
+    obj["description"] = { value: "LongAxis" };
+    obj["calculationResultCollection"] = {
+      CalculationResult: [this._createCalcResult(unit, "LongAxis", value)],
+    };
     this.imageAnnotations.ImageAnnotation[0].calculationEntityCollection[
       "CalculationEntity"
     ].push(obj);
     return uId;
   };
 
-  createShortAxisCalcEntity = shortAxis => {
+  createShortAxisCalcEntity = (shortAxis) => {
     let { unit, value } = shortAxis;
     var obj = {};
-    obj["calculationResultCollection"] = {
-      CalculationResult: [this._createCalcResult(unit, "ShortAxis", value)]
-    };
-    obj["description"] = { value: "ShortAxis" };
     const uId = generateUid();
     obj["uniqueIdentifier"] = { root: uId };
     obj["typeCode"] = [this._createTypeCode("G-A186", "SRT", "ShortAxis")];
+    obj["description"] = { value: "ShortAxis" };
+    obj["calculationResultCollection"] = {
+      CalculationResult: [this._createCalcResult(unit, "ShortAxis", value)],
+    };
     this.imageAnnotations.ImageAnnotation[0].calculationEntityCollection[
       "CalculationEntity"
     ].push(obj);
@@ -204,16 +207,16 @@ class Aim {
     var { unit, mean } = value;
     const { unit, typeCodeDcm } = this._getAimUnitAndDcmTypeCode(unit);
     var obj = {};
-    obj["calculationResultCollection"] = {
-      CalculationResult: [this._createCalcResult(unit, "Mean", mean, preLabel)]
-    };
-    obj["description"] = { value: "Mean" };
     const uId = generateUid();
     obj["uniqueIdentifier"] = { root: uId };
     obj["typeCode"] = [
       typeCodeDcm,
-      this._createTypeCode("R-00317", "SRT", "Mean")
+      this._createTypeCode("R-00317", "SRT", "Mean"),
     ];
+    obj["description"] = { value: "Mean" };
+    obj["calculationResultCollection"] = {
+      CalculationResult: [this._createCalcResult(unit, "Mean", mean, preLabel)],
+    };
     this.imageAnnotations.ImageAnnotation[0].calculationEntityCollection.CalculationEntity.push(
       obj
     );
@@ -225,18 +228,18 @@ class Aim {
     const { unit, typeCodeDcm } = this._getAimUnitAndDcmTypeCode(unit);
 
     var obj = {};
-    obj["calculationResultCollection"] = {
-      CalculationResult: [
-        this._createCalcResult(unit, "Standard Deviation", stdDev, preLabel)
-      ]
-    };
-    obj["description"] = { value: "Standard Deviation" };
     const uId = generateUid();
     obj["uniqueIdentifier"] = { root: uId };
     obj["typeCode"] = [
       typeCodeDcm,
-      this._createTypeCode("R-10047", "SRT", "Standard Deviation")
+      this._createTypeCode("R-10047", "SRT", "Standard Deviation"),
     ];
+    obj["description"] = { value: "Standard Deviation" };
+    obj["calculationResultCollection"] = {
+      CalculationResult: [
+        this._createCalcResult(unit, "Standard Deviation", stdDev, preLabel),
+      ],
+    };
     this.imageAnnotations.ImageAnnotation[0].calculationEntityCollection[
       "CalculationEntity"
     ].push(obj);
@@ -247,18 +250,18 @@ class Aim {
     var { unit, min } = value;
     const { unit, typeCodeDcm } = this._getAimUnitAndDcmTypeCode(unit);
     var obj = {};
-    obj["calculationResultCollection"] = {
-      CalculationResult: [
-        this._createCalcResult(unit, "Minimum", min, preLabel)
-      ]
-    };
-    obj["description"] = { value: "Minimum" };
     const uId = generateUid();
     obj["uniqueIdentifier"] = { root: uId };
     obj["typeCode"] = [
       typeCodeDcm,
-      this._createTypeCode("R-404FB", "SRT", "Minimum")
+      this._createTypeCode("R-404FB", "SRT", "Minimum"),
     ];
+    obj["description"] = { value: "Minimum" };
+    obj["calculationResultCollection"] = {
+      CalculationResult: [
+        this._createCalcResult(unit, "Minimum", min, preLabel),
+      ],
+    };
     this.imageAnnotations.ImageAnnotation[0].calculationEntityCollection[
       "CalculationEntity"
     ].push(obj);
@@ -269,31 +272,31 @@ class Aim {
     var { unit, max } = value;
     const { unit, typeCodeDcm } = this._getAimUnitAndDcmTypeCode(unit);
     var obj = {};
-    obj["calculationResultCollection"] = {
-      CalculationResult: [
-        this._createCalcResult(unit, "Maximum", max, preLabel)
-      ]
-    };
-    obj["description"] = { value: "Maximum" };
     const uId = generateUid();
     obj["uniqueIdentifier"] = { root: uId };
     obj["typeCode"] = [
       typeCodeDcm,
-      this._createTypeCode("G-A437", "SRT", "Maximum")
+      this._createTypeCode("G-A437", "SRT", "Maximum"),
     ];
+    obj["description"] = { value: "Maximum" };
+    obj["calculationResultCollection"] = {
+      CalculationResult: [
+        this._createCalcResult(unit, "Maximum", max, preLabel),
+      ],
+    };
     this.imageAnnotations.ImageAnnotation[0].calculationEntityCollection[
       "CalculationEntity"
     ].push(obj);
     return uId;
   };
 
-  _getAimUnitAndDcmTypeCode = unit => {
+  _getAimUnitAndDcmTypeCode = (unit) => {
     if (unit === "hu")
       return { unit: "[hnsf'U]", typeCodeDcm: this._createTypeCode() };
     else if (unit === "suv")
       return {
         unit: "{SUVbw}g/ml",
-        typeCodeDcm: this._createTypeCode(126401, "DCM", "SUVbw")
+        typeCodeDcm: this._createTypeCode(126401, "DCM", "SUVbw"),
       };
     return { unit, typeCodeDcm: this._createTypeCode() };
   };
@@ -301,15 +304,15 @@ class Aim {
   createVolumeCalcEntity = (value, preLabel) => {
     var { unit, volume } = value;
     var obj = {};
-    obj["calculationResultCollection"] = {
-      CalculationResult: [
-        this._createCalcResult(unit, "Volume", volume, preLabel)
-      ]
-    };
-    obj["description"] = { value: "Volume" };
     const uId = generateUid();
     obj["uniqueIdentifier"] = { root: uId };
     obj["typeCode"] = [this._createTypeCode("RID28668", "Radlex", "Volume")];
+    obj["description"] = { value: "Volume" };
+    obj["calculationResultCollection"] = {
+      CalculationResult: [
+        this._createCalcResult(unit, "Volume", volume, preLabel),
+      ],
+    };
     this.imageAnnotations.ImageAnnotation[0].calculationEntityCollection[
       "CalculationEntity"
     ].push(obj);
@@ -349,7 +352,7 @@ class Aim {
     );
   };
 
-  createCalculationEntityCollection = entities => {
+  createCalculationEntityCollection = (entities) => {
     var obj = {};
     obj["calculationEntityCollection"] = { CalculationEntity: entities };
     return obj;
@@ -361,13 +364,13 @@ class Aim {
 
   _createCoordinate = (coordinate, index) => {
     var obj = {};
-    obj["x"] = { value: coordinate.x };
     obj["coordinateIndex"] = { value: index };
+    obj["x"] = { value: coordinate.x };
     obj["y"] = { value: coordinate.y };
     return obj;
   };
 
-  _createCoordinateArray = points => {
+  _createCoordinateArray = (points) => {
     var coordinates = [];
     points.forEach((point, index) => {
       coordinates.push(this._createCoordinate(point, index));
@@ -381,23 +384,24 @@ class Aim {
       imageReferenceUid = imageReferenceUid.split("&frame=")[0]; //if multiframe strip the frame number from imageUID
 
     var obj = {};
-    obj["includeFlag"] = { value: true };
-    obj["twoDimensionSpatialCoordinateCollection"] = {
-      TwoDimensionSpatialCoordinate: this._createCoordinateArray(points)
-    };
     const uId = generateUid();
-    obj["shapeIdentifier"] = { value: shapeIndex };
-    obj["uniqueIdentifier"] = { root: uId };
     obj["xsi:type"] = type;
-    obj["imageReferenceUid"] = { root: imageReferenceUid };
-    obj["referencedFrameNumber"] = { value: frameNumber };
     this.imageAnnotations.ImageAnnotation[0].markupEntityCollection.MarkupEntity.push(
       obj
     );
+    obj["uniqueIdentifier"] = { root: uId };
+    obj["shapeIdentifier"] = { value: shapeIndex };
+    obj["includeFlag"] = { value: true };
+    obj["imageReferenceUid"] = { root: imageReferenceUid };
+    obj["referencedFrameNumber"] = { value: frameNumber };
+    obj["twoDimensionSpatialCoordinateCollection"] = {
+      TwoDimensionSpatialCoordinate: this._createCoordinateArray(points),
+    };
+
     return uId;
   };
 
-  _getFrameNumber = imageReferenceUid => {
+  _getFrameNumber = (imageReferenceUid) => {
     const frameNumber = imageReferenceUid.split("frame=");
     if (frameNumber.length > 1) return frameNumber[1];
     return 1;
@@ -413,7 +417,7 @@ class Aim {
         codeValue,
         codingSchemeDesignator,
         codeMeaning,
-        codingSchemeVersion
+        codingSchemeVersion,
       } = modalities[sopClassUid];
     else {
       const modality = this.temp.series.modality;
@@ -422,7 +426,7 @@ class Aim {
           codeValue,
           codingSchemeDesignator,
           codeMeaning,
-          codingSchemeVersion
+          codingSchemeVersion,
         } = modalities[modality];
       }
     }
@@ -431,7 +435,7 @@ class Aim {
     obj["codeSystemName"] = codingSchemeDesignator || "";
     obj["iso:displayName"] = {
       "xmlns:iso": "uri:iso.org:21090",
-      value: codeMeaning || ""
+      value: codeMeaning || "",
     };
     obj["codeSystemVersion"] = codingSchemeVersion || "";
     return obj;
@@ -440,7 +444,7 @@ class Aim {
   _createImageCollection = () => {
     let obj = {};
     obj["Image"] = [];
-    this.temp.image.forEach(image => {
+    this.temp.image.forEach((image) => {
       let { sopClassUid, sopInstanceUid } = image;
       sopClassUid = { root: sopClassUid };
       sopInstanceUid = { root: sopInstanceUid };
@@ -451,9 +455,9 @@ class Aim {
 
   _createImageSeries = () => {
     var obj = {};
+    obj["instanceUid"] = { root: this.temp.series.instanceUid };
     obj["modality"] = this._createModality();
     obj["imageCollection"] = this._createImageCollection();
-    obj["instanceUid"] = { root: this.temp.series.instanceUid };
     return obj;
   };
 
@@ -462,22 +466,22 @@ class Aim {
       accessionNumber,
       startTime,
       instanceUid,
-      startDate
+      startDate,
     } = this.temp.study;
     var obj = {};
-    obj["imageSeries"] = this._createImageSeries();
-    obj["startTime"] = { value: startTime };
     obj["instanceUid"] = { root: instanceUid };
     obj["startDate"] = { value: startDate };
+    obj["startTime"] = { value: startTime };
     obj["accessionNumber"] = { value: accessionNumber };
+    obj["imageSeries"] = this._createImageSeries();
     return obj;
   };
 
   _createImageReferenceEntity = () => {
     var obj = {};
-    obj["imageStudy"] = this._createImageStudy();
     obj["xsi:type"] = "DicomImageReferenceEntity";
     obj["uniqueIdentifier"] = { root: generateUid() };
+    obj["imageStudy"] = this._createImageStudy();
     return obj;
   };
 
@@ -490,14 +494,14 @@ class Aim {
   //
   //
   //
-  _createImageAnnotations = aimType => {
+  _createImageAnnotations = (aimType) => {
     const {
       name,
       comment,
       typeCode,
       imagingPhysicalEntityCollection,
       imagingObservationEntityCollection,
-      inferenceEntityCollection
+      inferenceEntityCollection,
     } = this.temp.aim;
     var obj = {};
     obj["uniqueIdentifier"] = { root: generateUid() };
@@ -513,7 +517,7 @@ class Aim {
       obj["calculationEntityCollection"] = { CalculationEntity: [] };
       obj["markupEntityCollection"] = { MarkupEntity: [] };
       obj["imageAnnotationStatementCollection"] = {
-        ImageAnnotationStatement: []
+        ImageAnnotationStatement: [],
       };
     }
     if (imagingObservationEntityCollection)
@@ -528,7 +532,7 @@ class Aim {
     return obj;
   };
 
-  _getComment = comment => {
+  _getComment = (comment) => {
     if (comment.value.length)
       comment.value = this._getProgrammedComment().concat("~~", comment.value);
     else comment.value = this._getProgrammedComment();
@@ -557,8 +561,8 @@ class Aim {
       ? (references = "CalculationEntityReferencesMarkupEntityStatement")
       : (references = "CalculationEntityReferencesSegmentationEntityStatement");
     obj["xsi:type"] = references;
-    obj["objectUniqueIdentifier"] = { root: objectId };
     obj["subjectUniqueIdentifier"] = { root: subjectId };
+    obj["objectUniqueIdentifier"] = { root: objectId };
     this.imageAnnotations.ImageAnnotation[0].imageAnnotationStatementCollection.ImageAnnotationStatement.push(
       obj
     );
@@ -592,17 +596,17 @@ class Aim {
   /*    Segmentation Entitiy Realted Functions      */
   /*                                                */
 
-  createSegmentationEntity = segmentation => {
+  createSegmentationEntity = (segmentation) => {
     var obj = {};
     obj["referencedSopInstanceUid"] = {
-      root: segmentation.referencedSopInstanceUid
+      root: segmentation.referencedSopInstanceUid,
     };
     obj["segmentNumber"] = { value: 1 };
     obj["seriesInstanceUid"] = {
-      root: segmentation.seriesInstanceUid
+      root: segmentation.seriesInstanceUid,
     };
     obj["studyInstanceUid"] = {
-      root: segmentation.studyInstanceUid
+      root: segmentation.studyInstanceUid,
     };
     obj["xsi:type"] = "DicomSegmentationEntity";
     obj["sopClassUid"] = { root: "1.2.840.10008.5.1.4.1.1.66.4" };
@@ -620,40 +624,40 @@ class Aim {
   //
   //
   // Person
-  _createPerson = person => {
+  _createPerson = (person) => {
     const { sex, name, patientId, birthDate } = person;
     return {
-      sex: { value: sex },
       name: { value: name },
       id: { value: patientId },
-      birthDate: { value: birthDate }
+      birthDate: { value: birthDate },
+      sex: { value: sex },
     };
   };
 
   //
   //
   // Eqipment
-  _createEquipment = equipment => {
+  _createEquipment = (equipment) => {
     const {
       manufacturerName,
       manufacturerModelName,
-      softwareVersion
+      softwareVersion,
     } = equipment;
     return {
       manufacturerName: { value: manufacturerName },
       manufacturerModelName: { value: manufacturerModelName },
-      softwareVersion: { value: softwareVersion }
+      softwareVersion: { value: softwareVersion },
     };
   };
 
   //
   //
   // User
-  _createUser = user => {
+  _createUser = (user) => {
     const { loginName, name } = user;
     return {
+      name: { value: name },
       loginName: { value: loginName },
-      name: { value: name }
     };
   };
 
