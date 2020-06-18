3ae68447dfc60daf852d164ccd4fa08e8dbdbba6
Ozge Yurtsever <ozge.ikiz.yurtsever@gmail.com>
2020-06-18 00:46:36 -0700

    Add Image numbers next to the annotation name

--- src/components/annotationsList/annotationDock/annotationsLink.jsx
+++ src/components/annotationsList/annotationDock/annotationsLink.jsx
@@ -6,26 +6,60 @@ const annotationsLink = props => {
   const { openSeries, activePort, aimsList } = props;
   const { seriesUID } = openSeries[activePort];
   let list = [];
+  let header = [];
   if (aimsList[seriesUID]) {
     const seriesAims = Object.values(aimsList[seriesUID]);
+    header = [
+      <th
+        className="annsLink-table __header --cell"
+        key="anns-header"
+        id="annsHeader"
+      >
+        Description
+      </th>,
+      <th
+        className="annsLink-table __header --cell"
+        key="slide-header"
+        id="annsImgSlide"
+      >
+        img
+      </th>,
+    ];
+
     list = seriesAims.map((aim, i) => {
       if (!props.imageAims[aim.id]) {
+        let { comment } = aim.json;
+        comment = Object.values(comment);
+        const slideNo = comment.length > 0 ? comment[0].split("/")[2] : "";
         return (
-          <div
-            key={aim.id}
-            className="annotationsLink"
-            data-id={aim.id}
-            data-serie={seriesUID}
-            onClick={props.jumpToAim}
-          >
-            {aim.name}
-          </div>
+          <tr key={aim.id} className="annsLink-table __tbody --row">
+            <td
+              data-id={aim.id}
+              data-serie={seriesUID}
+              onClick={props.jumpToAim}
+              className="annsLink-table __tbody --cell"
+            >
+              {aim.name}
+            </td>
+            <td className="annsLink-table __tbody --cell2">{slideNo}</td>
+          </tr>
         );
       }
     });
   }
 
-  return <React.Fragment>{list}</React.Fragment>;
+  return (
+    <React.Fragment>
+      {aimsList[seriesUID] && (
+        <table className="annsLink-table">
+          <thead className="annsLink-table __header">
+            <tr className="annsLink-table __header --row">{header}</tr>
+          </thead>
+          <tbody className="annsLink-table __tbody">{list}</tbody>
+        </table>
+      )}
+    </React.Fragment>
+  );
 };
 
 const mapStateToProps = state => {
