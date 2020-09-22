fda948631119d3aceaa23cd9b3433e3ecf3008d8
cavit <cavitaltindag@gmail.com>
2020-09-21 22:44:55 -0700

    axios removed. Pooling used. docker log send to a writable stream

--- src/components/management/plugins/tabs/track/trackTab.jsx
+++ src/components/management/plugins/tabs/track/trackTab.jsx
@@ -10,6 +10,7 @@ import {
   FaEye,
   FaTrash,
   FaEnvelopeOpenText,
+  FaWindowClose,
 } from "react-icons/fa";
 
 import {
@@ -22,7 +23,9 @@ import {
   stopContainerLog,
 } from "../../../../../services/pluginServices";
 import "./../../css/plugin.css";
-import axios from "axios";
+import "../../../menuStyle.css";
+// import axios from "axios";
+import Draggable from "react-draggable";
 // import cancelXhrAdapter from "axios-cancel";
 // import { Cancellation } from "axios-cancel/cancel";
 class TrackTab extends React.Component {
@@ -194,127 +197,123 @@ class TrackTab extends React.Component {
     }
   };
 
+  // handleGetContainerLog = async (dataOriginal) => {
+  //   const _this = this;
+  //   if (this.state.runungLogId === 0) {
+  //     console.log("data original ", dataOriginal);
+
+  //     //  const containerLoggingIntervalHolder = null;
+
+  //     const containerid = dataOriginal.id;
+
+  //     console.log("get logs called :", containerid);
+  //     // let containerlog = await getContainerLog(containerid);
+  //     // if (this.state.showContainerLog == true) {
+  //     const CancelToken = axios.CancelToken;
+  //     const source = CancelToken.source();
+  //     this.setState({
+  //       runungLogId: containerid,
+  //       showContainerLog: true,
+  //       source: source,
+  //     });
+  //     try {
+  //       let rea = await axios({
+  //         url: "http://c02s30xdg8wl.local:8080/" + "container/",
+  //         data: dataOriginal,
+  //         method: "POST",
+  //         //cancelToken: source.token,
+  //         // adapter: cancelXhrAdapter,
+  //         // cancellation: this.state.cancellation,
+  //         cancelToken: new CancelToken(function executor(c) {
+  //           // An executor function receives a cancel function as a parameter
+  //           _this.setState({ ocancel: c });
+  //           // cancel = c;
+  //         }),
+  //         onDownloadProgress: (progressEvent) => {
+  //           const dataChunk = progressEvent.currentTarget.response;
+
+  //           console.log(dataChunk);
+  //           this.setState({
+  //             containerLogData: dataChunk,
+  //           });
+  //           // dataChunk contains the data that have been obtained so far (the whole data so far)..
+  //           // So here we do whatever we want with this partial data..
+  //           // In my case I'm storing that on a redux store that is used to
+  //           // render a table, so now, table rows are rendered as soon as
+  //           // they are obtained from the endpoint.
+  //         },
+  //       });
+  //     } catch (err) {
+  //       console.log("mu mu mu :", err);
+  //     }
+
+  //     // }
+
+  //     // console.log(" container log returned value ", containerlog);
+
+  //     // const containerLoggingIntervalHolder = setInterval(
+  //     //   async function (o) {
+  //     //     // method to be executed;
+
+  //     //     let status = 202;
+  //     //     console.log("continer id = ", containerid);
+  //     //     containerlog = await getContainerLog(dataOriginal);
+  //     //     console.log("logaaaaa : ", containerlog);
+  //     //     console.log("container status :", containerlog.status);
+  //     //     status = containerlog.status;
+  //     //     o.setState({
+  //     //       containerLogData: containerlog.data,
+  //     //     });
+
+  //     //     console.log("stream return : ", o.state.containerLogData);
+  //     //   },
+  //     //   5000,
+  //     //   _this
+  //     // );
+  //     // this.setState({
+  //     //   containerLoggingIntervalHandle: containerLoggingIntervalHolder,
+  //     //   showContainerLog: true,
+  //     // });
+  //   }
+  // };
+
   handleGetContainerLog = async (dataOriginal) => {
-    const _this = this;
-    if (this.state.runungLogId === 0) {
-      console.log("data original ", dataOriginal);
-
-      //  const containerLoggingIntervalHolder = null;
-
-      const containerid = dataOriginal.id;
-
-      console.log("get logs called :", containerid);
-      // let containerlog = await getContainerLog(containerid);
-      // if (this.state.showContainerLog == true) {
-      const CancelToken = axios.CancelToken;
-      const source = CancelToken.source();
-      this.setState({
-        runungLogId: containerid,
-        showContainerLog: true,
-        source: source,
-      });
-      try {
-        let rea = await axios({
-          url: "http://c02s30xdg8wl.local:8080/" + "container/",
-          data: dataOriginal,
-          method: "POST",
-          //cancelToken: source.token,
-          // adapter: cancelXhrAdapter,
-          // cancellation: this.state.cancellation,
-          cancelToken: new CancelToken(function executor(c) {
-            // An executor function receives a cancel function as a parameter
-            _this.setState({ ocancel: c });
-            // cancel = c;
-          }),
-          onDownloadProgress: (progressEvent) => {
-            const dataChunk = progressEvent.currentTarget.response;
-
-            console.log(dataChunk);
-            this.setState({
-              containerLogData: dataChunk,
-            });
-            // dataChunk contains the data that have been obtained so far (the whole data so far)..
-            // So here we do whatever we want with this partial data..
-            // In my case I'm storing that on a redux store that is used to
-            // render a table, so now, table rows are rendered as soon as
-            // they are obtained from the endpoint.
-          },
+    if (this.state.showContainerLog === true) {
+      clearInterval(this.state.containerLoggingIntervalHandle);
+    }
+    //  const containerLoggingIntervalHolder = null;
+    const containerid = dataOriginal.id;
+    let _this = this;
+    try {
+      let containerlog = await getContainerLog(dataOriginal);
+      console.log("err stream", containerlog);
+      if (containerlog.data != 404) {
+        this.setState({
+          showContainerLog: true,
+          containerLogData: containerlog.data,
         });
-      } catch (err) {
-        console.log("mu mu mu :", err);
+
+        const containerLoggingIntervalHolder = setInterval(
+          async function (innerThis) {
+            console.log("continer id = ", containerid);
+            containerlog = await getContainerLog(dataOriginal);
+            innerThis.setState({
+              containerLogData: containerlog.data,
+            });
+          },
+          5000,
+          _this
+        );
+        this.setState({
+          containerLoggingIntervalHandle: containerLoggingIntervalHolder,
+        });
+      } else {
+        alert("Please start plugin first");
       }
-
-      // }
-
-      // console.log(" container log returned value ", containerlog);
-
-      // const containerLoggingIntervalHolder = setInterval(
-      //   async function (o) {
-      //     // method to be executed;
-
-      //     let status = 202;
-      //     console.log("continer id = ", containerid);
-      //     containerlog = await getContainerLog(dataOriginal);
-      //     console.log("logaaaaa : ", containerlog);
-      //     console.log("container status :", containerlog.status);
-      //     status = containerlog.status;
-      //     o.setState({
-      //       containerLogData: containerlog.data,
-      //     });
-
-      //     console.log("stream return : ", o.state.containerLogData);
-      //   },
-      //   5000,
-      //   _this
-      // );
-      // this.setState({
-      //   containerLoggingIntervalHandle: containerLoggingIntervalHolder,
-      //   showContainerLog: true,
-      // });
+    } catch (err) {
+      alert(err);
     }
   };
-
-  // handleGetContainerLog = async (dataOriginal) => {
-  //   console.log("data original ", dataOriginal);
-  //   if (this.state.showContainerLog === true) {
-  //     clearInterval(this.state.containerLoggingIntervalHandle);
-  //   }
-  //   //  const containerLoggingIntervalHolder = null;
-  //   const containerid = dataOriginal.id;
-  //   console.log("f ned ", containerid);
-  //   let _this = this;
-
-  //   console.log("get logs called :", containerid);
-  //   let containerlog = await getContainerLog(dataOriginal);
-  //   this.setState({
-  //     showContainerLog: true,
-  //     containerLogData: containerlog.data,
-  //   });
-
-  //   const containerLoggingIntervalHolder = setInterval(
-  //     async function (o) {
-  //       // method to be executed;
-
-  //       let status = 202;
-  //       console.log("continer id = ", containerid);
-  //       containerlog = await getContainerLog(dataOriginal);
-  //       console.log("logaaaaa : ", containerlog);
-  //       console.log("container status :", containerlog.status);
-  //       status = containerlog.status;
-  //       o.setState({
-  //         containerLogData: containerlog.data,
-  //       });
-
-  //       console.log("stream return : ", o.state.containerLogData);
-  //     },
-  //     5000,
-  //     _this
-  //   );
-  //   this.setState({
-  //     containerLoggingIntervalHandle: containerLoggingIntervalHolder,
-  //     showContainerLog: true,
-  //   });
-  // };
   handleDetachLog = async (dataOriginal) => {
     console.log("data original ", dataOriginal);
 
@@ -331,13 +330,13 @@ class TrackTab extends React.Component {
     // if (this.state.showContainerLog == true) {
   };
   handlerStopContainerLogging = () => {
-    this.state.ocancel();
+    // this.state.ocancel();
     this.setState({
       showContainerLog: false,
       containerLogData: "",
       runungLogId: 0,
     });
-    // clearInterval(this.state.containerLoggingIntervalHandle);
+    clearInterval(this.state.containerLoggingIntervalHandle);
   };
 
   showRuntimeParamsForNonZero = (data) => {
@@ -674,6 +673,11 @@ class TrackTab extends React.Component {
       },
     ];
   };
+
+  pluginWindowClickHandler = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+  };
   render() {
     return (
       <div>
@@ -741,36 +745,36 @@ class TrackTab extends React.Component {
         )}
 
         {this.state.showContainerLog && (
-          <div>
-            <div className="tools menu-display" id="template">
-              <Modal.Dialog className="create-plugin__modal">
-                <Modal.Header>
-                  <Modal.Title>Runtime Parameters</Modal.Title>
-                </Modal.Header>
-                <Modal.Body className="create-user__modal--body">
-                  <textarea
-                    value={this.state.containerLogData}
-                    rows={5}
-                    cols={100}
-                  ></textarea>
-                </Modal.Body>
-
-                <Modal.Footer className="create-user__modal--footer">
-                  <div className="create-user__modal--buttons">
-                    <button
-                      variant="secondary"
-                      className="btn btn-sm btn-outline-light"
-                      onClick={() => {
-                        this.handlerStopContainerLogging();
-                      }}
-                    >
-                      close
-                    </button>
-                  </div>
-                </Modal.Footer>
-              </Modal.Dialog>
+          <Draggable
+            onClick={this.pluginWindowClickHandler}
+            onMouseDown={this.pluginWindowClickHandler}
+            onMouseMove={this.pluginWindowClickHandler}
+          >
+            <div
+              className="pluginlogpopup"
+              id="pluginlogpopup"
+              onClick={this.pluginWindowClickHandler}
+              onMouseDown={this.pluginWindowClickHandler}
+              onMouseMove={this.pluginWindowClickHandler}
+            >
+              <div className="pluginCloseButtonHeader">
+                <FaWindowClose
+                  className="pluginCloseButton"
+                  onClick={() => {
+                    this.handlerStopContainerLogging();
+                  }}
+                />
+              </div>
+              <div>
+                <textarea
+                  className="pluginLogTextArea"
+                  value={this.state.containerLogData}
+                  rows={15}
+                  cols={150}
+                ></textarea>
+              </div>
             </div>
-          </div>
+          </Draggable>
         )}
       </div>
     );
