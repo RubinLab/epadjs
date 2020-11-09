67c64a656707452df99485b835ede86526e05052
OzgeYurtsever <ozge.ikiz.yurtsever@gmail.com>
2020-11-09 14:01:56 -0800

    Create a generic popup in plugin dir

---
+++ src/components/management/plugins/common/Popup.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import Draggable from 'react-draggable';
+import '../css/plugin.css';
+import '../../menuStyle.css';
+
+
+export default function Popup({ children }) {
+  const pluginWindowClickHandler = e => {
+    e.preventDefault();
+    e.stopPropagation();
+  };
+
+  return (
+    <Draggable
+      onClick={pluginWindowClickHandler}
+      onMouseDown={pluginWindowClickHandler}
+      onMouseMove={pluginWindowClickHandler}
+    >
+      <div
+        id="plugin-common-popup"
+        // className="pluginlogpopup"
+        onClick={pluginWindowClickHandler}
+        onMouseDown={pluginWindowClickHandler}
+        onMouseMove={pluginWindowClickHandler}
+      >
+          {children}
+      </div>
+    </Draggable>
+  );
+}
