86f50f2de4a286c4b1d39af547ed3ab28e392844
OzgeYurtsever <ozge.ikiz.yurtsever@gmail.com>
2021-04-13 11:57:04 -0700

    Implement authService class and methods for oidc lib

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,4 +1,8 @@
 "use strict";
+
+import { IDENTITY_CONFIG, METADATA_OIDC } from "../Utils/authConst";
+import { UserManager, WebStorageStateStore, Log } from "oidc-client";
+
 import * as cornerstoneWADOImageLoader from "cornerstone-wado-image-loader";
 
 const apiUrl = sessionStorage.getItem("apiUrl");
@@ -10,7 +14,7 @@ export function refreshToken(keycloak, minValidity) {
   return new Promise((resolve, reject) => {
     keycloak
       .updateToken(minValidity)
-      .success(function (refreshed) {
+      .success(function(refreshed) {
         if (refreshed) {
           // console.log("Token was successfully refreshed");
         } else {
@@ -18,7 +22,7 @@ export function refreshToken(keycloak, minValidity) {
         }
         resolve();
       })
-      .error(function () {
+      .error(function() {
         reject();
       });
   });
@@ -52,9 +56,9 @@ export async function getAuthHeader() {
       const header = `Bearer ${this.keycloak.token}`;
       if (header) {
         cornerstoneWADOImageLoader.configure({
-          beforeSend: function (xhr) {
+          beforeSend: function(xhr) {
             xhr.setRequestHeader("Authorization", header);
-          },
+          }
         });
         return header;
       }
@@ -65,10 +69,113 @@ export async function getAuthHeader() {
   return undefined;
 }
 
+class AuthService {
+  UserManager;
+
+  constructor() {
+    this.UserManager = new UserManager({
+      ...IDENTITY_CONFIG,
+      userStore: new WebStorageStateStore({ store: window.sessionStorage }),
+      metadata: {
+        ...METADATA_OIDC
+      }
+    });
+    // Logger
+    Log.logger = console;
+    Log.level = Log.DEBUG;
+    this.UserManager.events.addUserLoaded(user => {
+      if (window.location.href.indexOf("signin-oidc") !== -1) {
+        this.navigateToScreen();
+      }
+    });
+
+    this.UserManager.events.addSilentRenewError(e => {
+      console.log("silent renew error", e.message);
+    });
+
+    this.UserManager.events.addAccessTokenExpired(() => {
+      console.log("token expired");
+      this.signinSilent();
+    });
+  }
+
+  signinRedirectCallback = () => {
+    this.UserManager.signinRedirectCallback().then(() => {
+      "";
+    });
+  };
+
+  getUser = async () => {
+    const user = await this.UserManager.getUser();
+    if (!user) {
+      return await this.UserManager.signinRedirectCallback();
+    }
+    return user;
+  };
+
+  parseJwt = token => {
+    const base64Url = token.split(".")[1];
+    const base64 = base64Url.replace("-", "+").replace("_", "/");
+    return JSON.parse(window.atob(base64));
+  };
+
+  signinRedirect = () => {
+    localStorage.setItem("redirectUri", window.location.pathname);
+    this.UserManager.signinRedirect({});
+  };
+
+  navigateToScreen = () => {
+    window.location.replace("/en/dashboard");
+  };
+
+  isAuthenticated = () => {
+    const oidcStorage = JSON.parse(
+      sessionStorage.getItem(
+        `oidc.user:${process.env.REACT_APP_AUTH_URL}:${process.env.REACT_APP_IDENTITY_CLIENT_ID}`
+      )
+    );
+
+    return !!oidcStorage && !!oidcStorage.access_token;
+  };
+
+  signinSilent = () => {
+    this.UserManager.signinSilent()
+      .then(user => {
+        console.log("signed in", user);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+  signinSilentCallback = () => {
+    this.UserManager.signinSilentCallback();
+  };
+
+  createSigninRequest = () => {
+    return this.UserManager.createSigninRequest();
+  };
+
+  logout = () => {
+    this.UserManager.signoutRedirect({
+      id_token_hint: localStorage.getItem("id_token")
+    });
+    this.UserManager.clearStaleState();
+  };
+
+  signoutRedirectCallback = () => {
+    this.UserManager.signoutRedirectCallback().then(() => {
+      localStorage.clear();
+      window.location.replace(process.env.REACT_APP_PUBLIC_URL);
+    });
+    this.UserManager.clearStaleState();
+  };
+}
+
 export default {
   login,
   logout,
   getCurrentUser,
   getAuthHeader,
   refreshToken,
+  AuthService
 };
