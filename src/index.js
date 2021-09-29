79ec067caf1af58fc12e5d05f810809b1dd8a987
OzgeYurtsever <ozge.ikiz.yurtsever@gmail.com>
2021-09-29 13:59:56 -0700

    Add service-worker related files and libs

--- src/index.js
+++ src/index.js
@@ -7,9 +7,12 @@ import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
+// import registerServiceWorker from "./registerServiceWorker";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import "font-awesome/css/font-awesome.css";
 import rootReducer from "./reducers";
+import reportWebVitals from './reportWebVitals';
+
 
 const store = createStore(
   rootReducer,
@@ -25,4 +28,14 @@ render(
   </BrowserRouter>,
   document.getElementById("root")
 );
-registerServiceWorker();
+// registerServiceWorker();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
