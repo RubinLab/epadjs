ebd0607cd8e61d09abe36a03e2a81662c10a9989
Mete Akdogan <muakdogan@gmail.com>
2022-10-08 08:08:00 -0700

    handles examTypes of sectra

--- src/components/annotationsList/selectSerieModal.jsx
+++ src/components/annotationsList/selectSerieModal.jsx
@@ -371,6 +371,12 @@ class selectSerieModal extends React.Component {
     answers.name.value = "Teaching File";
     const { data: study } = await getSingleStudy(studyUID);
     console.log("Study", study);
+    let examTypes;
+    ({ examTypes } = study);
+    if (examTypes?.length === 1 && examTypes[0].includes("\\")) {
+      examTypes = examTypes[0].split("\\");
+    }
+    console.log("Exam types", examTypes);
     const aimData = { study, answers, user: getUserForAim() };
     const aim = new Aim(
       aimData,
