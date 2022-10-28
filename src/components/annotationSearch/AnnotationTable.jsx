a6fdea88f2644e84ba6cf4d44cce9284787de71f
Mete Akdogan <muakdogan@gmail.com>
2022-10-28 07:35:01 -0700

    pass compMod to row

--- src/components/annotationSearch/AnnotationTable.jsx
+++ src/components/annotationSearch/AnnotationTable.jsx
@@ -483,7 +483,7 @@ function AnnotationTable(props) {
         {
           Header: 'Modality',
           accessor: 'modality',
-          Cell: ({ row: { original: { modality } } }) => {
+          Cell: (compModality, { row: { original: { modality } } }) => {
             console.log("In row", modality, compModality[modality]);
             if (compModality[modality]) return <div className={'modality-capital'}>{compModality[modality]}</div>;
             else return <div className={'modality-capital'}>{modality}</div>;
