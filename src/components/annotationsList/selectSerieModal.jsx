c182c77c652f6a410b96c8cb789ea22cce5886bb
Mete Akdogan <muakdogan@gmail.com>
2022-11-09 16:10:53 -0800

    catches series change dd error

--- src/components/annotationsList/selectSerieModal.jsx
+++ src/components/annotationsList/selectSerieModal.jsx
@@ -370,15 +370,12 @@ class selectSerieModal extends React.Component {
     const answers = this.semanticAnswers.saveAim();
     answers.name.value = "Teaching File";
     const { data: study } = await getSingleStudy(studyUID);
-    console.log("Study", study);
     let examTypes;
     ({ examTypes } = study);
     if (examTypes?.length === 1 && examTypes[0].includes("\\")) {
       examTypes = examTypes[0].split("\\");
     }
-    console.log("Exam types", examTypes);
     study.examTypes = examTypes.filter(type => DISP_MODALITIES.includes(type));
-    console.log("Study after", study);
     const aimData = { study, answers, user: getUserForAim() };
     const aim = new Aim(
       aimData,
