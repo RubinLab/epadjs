c3feb36ea5ccb564fa5b45b06da6ee331e2d88e1
Mete Akdogan <muakdogan@gmail.com>
2022-11-15 17:50:33 -0800

    updates study aim modality save

--- src/components/aimEditor/aimEditor.jsx
+++ src/components/aimEditor/aimEditor.jsx
@@ -442,6 +442,12 @@ class AimEditor extends Component {
       if (isStudyAim) { //Study annotation
         const { openSeries, activePort } = this.props;
         const { data: study } = await getSingleStudy(openSeries[activePort].studyUID);
+        let examTypes;
+        ({ examTypes } = study);
+        if (examTypes?.length === 1 && examTypes[0].includes("\\")) {
+          examTypes = examTypes[0].split("\\");
+        }
+        study.examTypes = examTypes.filter(type => DISP_MODALITIES.includes(type));
         aimData = { study, answers, user };
         aim = new Aim(
           aimData,
