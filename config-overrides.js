3cc2b8ed328ddd70dcd1f1f7af4db26fee57556a
OzgeYurtsever <ozge.ikiz.yurtsever@gmail.com>
2023-03-03 11:33:27 -0800

    Remove individual libraries

--- config-overrides.js
+++ config-overrides.js
@@ -2,18 +2,18 @@ const webpack = require('webpack');
 module.exports = function override(config) {
     const fallback = config.resolve.fallback || {};
     Object.assign(fallback, {
-        "path": require.resolve("path-browserify"),
-        "crypto": require.resolve("crypto-browserify"),
-        "stream": require.resolve("stream-browserify"),
-        "assert": require.resolve("assert"),
-        "http": require.resolve("stream-http"),
-        "https": require.resolve("https-browserify"),
-        "os": require.resolve("os-browserify"),
-        "url": require.resolve("url"),
+        // "path": require.resolve("path-browserify"),
+        // "crypto": require.resolve("crypto-browserify"),
+        // "stream": require.resolve("stream-browserify"),
+        // "assert": require.resolve("assert"),
+        // "http": require.resolve("stream-http"),
+        // "https": require.resolve("https-browserify"),
+        // "os": require.resolve("os-browserify"),
+        // "url": require.resolve("url"),
         // "process": require.resolve("process"),
         // "fs": false,
-        "fs": require.resolve('browserify-fs'),
-        "buffer": require.resolve("buffer/"),
+        // "fs": require.resolve('browserify-fs'),
+        // "buffer": require.resolve("buffer/"),
         // "process": require.resolve('process/browser'),  // <- this
 
     })
