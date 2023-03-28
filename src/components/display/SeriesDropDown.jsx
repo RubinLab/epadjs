49a9ad28ae6c9f6b088850113b309563e2c31428
OzgeYurtsever <ozge.ikiz.yurtsever@gmail.com>
2023-03-27 23:48:15 -0700

    Replace wordy dropdown with an icon

--- src/components/display/SeriesDropDown.jsx
+++ src/components/display/SeriesDropDown.jsx
@@ -3,10 +3,13 @@ import React, { useState, useEffect } from 'react';
 import { connect } from "react-redux";
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
+import ReactTooltip from "react-tooltip";
+import { TbStack2 } from "react-icons/tb";
 import { getStudyAims } from '../../services/studyServices';
 import { getSeries } from '../../services/seriesServices';
 import { addStudyToGrid, replaceInGrid, getSingleSerie, clearActivePortAimID } from 'components/annotationsList/action';
 import "./SeriesDropDown.css";
+
 
 const SeriesDropDown = (props) => {
     const [seriesList, setSeriesList] = useState([]);
@@ -63,7 +66,10 @@ const SeriesDropDown = (props) => {
                 id={`dropdown-button-drop-1`}
                 size="sm"
                 variant="secondary"
-                title="Other Series"
+                // title="Other Series"
+                title={<TbStack2 />}
+                data-tip
+                data-for="dropdownOtherSeries"
             >
                 {seriesList && seriesList.length && seriesList.map(({ seriesDescription, seriesUID, seriesNo, i }) => {
                     let isCurrent = props.openSeries[props.activePort].seriesUID === seriesUID;
@@ -71,6 +77,14 @@ const SeriesDropDown = (props) => {
                     return (<Dropdown.Item key={seriesUID} eventKey={seriesUID} onSelect={handleSelect} style={{ textAlign: "left !important" }}>{seriesNo ? seriesNo : "#NA"} {counts} {seriesDescription?.length ? seriesDescription : "No Description"} {isCurrent ? "(Current)" : ""}</Dropdown.Item>);
                 })}
             </DropdownButton>
+            <ReactTooltip
+                id="dropdownOtherSeries"
+                place="bottom"
+                type="info"
+                delayShow={1000}
+            >
+                <span>Other Series</span>
+            </ReactTooltip>
         </div >
     );
 }
