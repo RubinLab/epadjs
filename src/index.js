34d80e99d6670f2f37e16be13c70539d122b9112
James <jamesrobertson527@gmail.com>
2023-05-17 14:24:42 -0700

    Swapped right tab button positions

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ const store = createStore(
 
 /* eslint-enable */
 render(
-  <BrowserRouter basename={`${process.env.REACT_APP_BASE_URL}`}>
+  <BrowserRouter>
     <Provider store={store}>
       <App />
     </Provider>
