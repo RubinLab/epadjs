d9e19bf41cd00b407b4ad2143a02932aabe1b1eb
OzgeYurtsever <ozge.ikiz.yurtsever@gmail.com>
2023-06-24 22:54:40 -0700

    Add try and catch for deleteStudy

--- src/components/annotationsList/selectSerieModal.jsx
+++ src/components/annotationsList/selectSerieModal.jsx
@@ -218,14 +218,18 @@ class selectSerieModal extends React.Component {
     });
     // this.props.dispatch(clearSelection());
     this.props.onCancel();
-    if (this.mode === 'teaching' && this.props.decrArgs) {
-      const { projectID, patientID, studyUID } = this.props.decrArgs;
-      await deleteStudy({ projectID, patientID, studyUID }, '?all=true');
-      alert(
-        "Teaching file couldn't be saved! More information about the error can be found in the logs."
-      );
-      console.error(error);
-      this.setState({ isButtonDisabled: false });
+    try {
+      if (this.mode === 'teaching' && this.props.decrArgs) {
+        const { projectID, patientID, studyUID } = this.props.decrArgs;
+        await deleteStudy({ projectID, patientID, studyUID }, '?all=true');
+        alert(
+          "Teaching file couldn't be saved! More information about the error can be found in the logs."
+        );
+        console.error(error);
+        this.setState({ isButtonDisabled: false });
+      }
+    } catch (err) {
+      console.error(err);
     }
   };
 
