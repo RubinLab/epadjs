29a23527873690f9f71772a1ada910d1ca485f5c
Shannon <38598412+swang1111@users.noreply.github.com>
2023-07-11 00:35:04 -0700

    Create UploadCSV component

--- src/components/csv2aim/csv2aim.jsx
+++ src/components/csv2aim/csv2aim.jsx
@@ -1,6 +1,6 @@
 import React, { Component, useState } from "react";
 import { BiUpload } from 'react-icons/bi';
-import UploadModal from '../searchView/uploadModal';
+import UploadCSV from './uploadcsv';
 
 export default function CSV2AIM() {
     const [uploadClicked, setUploadClicked] = useState(false);
@@ -13,7 +13,7 @@ export default function CSV2AIM() {
         <>
             <button type="button" className="btn btn-sm" onClick={() => { setUploadClicked(true) }}><BiUpload /><br />Convert CSV to AIM files</button>
             {uploadClicked && (
-            <UploadModal
+            <UploadCSV
                 onCancel={() => setUploadClicked(false)}
                 onResolve={handleSubmitUpload}
             />
---
+++ src/components/csv2aim/uploadcsv.jsx
@@ -0,0 +1,197 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { Modal, Button } from 'react-bootstrap';
+import {
+  getProjects,
+  uploadFileToProject
+} from '../../services/projectServices';
+import { uploadFileToSubject } from '../../services/subjectServices';
+import { uploadFileToStudy } from '../../services/studyServices';
+import { uploadFileToSeries } from '../../services/seriesServices';
+import { getTemplates } from '../annotationsList/action';
+
+let mode;
+
+class UploadCSV extends React.Component {
+  mode = sessionStorage.getItem('mode');
+  state = {
+    tiff: false,
+    osirix: false,
+    projects: [],
+    files: [],
+    projectID: ''
+  };
+
+  onSelect = e => {
+    const { name, checked } = e.target;
+    this.setState({ [name]: checked });
+  };
+
+  componentDidMount = async () => {
+    try {
+      const { pid } = this.props;
+      if (mode !== 'lite') {
+        let { data: projects } = await getProjects();
+        for (let i = 0; i < projects.length; i++) {
+          if (projects[i].id === 'all') {
+            projects.splice(i, 1);
+            i = i - 1;
+            continue;
+          }
+          if (projects[i].id === 'nonassigned') {
+            projects.splice(i, 1);
+            i = i - 1;
+            continue;
+          }
+        }
+
+        const nonSelectablePid = pid === 'nonassigned' || pid === 'all';
+        const projectID =
+          nonSelectablePid && projects.length > 0
+            ? projects[0].id
+            : !nonSelectablePid
+              ? pid
+              : '';
+        this.setState({ projects, projectID });
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  onSelectFile = e => {
+    this.setState({ files: Array.from(e.target.files) });
+  };
+
+  onUpload = () => {
+    let {
+      selectedPatients,
+      selectedStudies,
+      selectedSeries,
+      clearTreeData,
+      onResolve,
+      onCancel,
+      onSubmit,
+      clearTreeExpand
+    } = this.props;
+    selectedPatients = Object.values(selectedPatients);
+    selectedStudies = Object.values(selectedStudies);
+    selectedSeries = Object.values(selectedSeries);
+
+    const promises = [];
+    const projectID = this.props.projectID
+      ? this.props.projectID
+      : this.state.projectID;
+    const formData = new FormData();
+    this.state.files.forEach((file, index) => {
+      formData.append(`file${index + 1}`, file);
+    });
+    const config = {
+      headers: {
+        'content-type': 'multipart/form-data'
+      }
+    };
+
+    if (onSubmit) onSubmit();
+    if (selectedPatients.length > 0) {
+      selectedPatients.forEach(el =>
+        promises.push(uploadFileToSubject(formData, config, el))
+      );
+    } else if (selectedStudies.length > 0) {
+      selectedStudies.forEach(el =>
+        promises.push(uploadFileToStudy(formData, config, el))
+      );
+    } else if (selectedSeries.length > 0) {
+      selectedSeries.forEach(el =>
+        promises.push(uploadFileToSeries(formData, config, el))
+      );
+    } else {
+      promises.push(uploadFileToProject(formData, config, projectID));
+    }
+
+    Promise.all(promises)
+      .then(() => {
+        if (clearTreeData) {
+          // clearTreeData();
+          localStorage.setItem('treeData', JSON.stringify({}));
+          clearTreeExpand();
+        }
+        this.props.dispatch(getTemplates());
+        if (onResolve) onResolve();
+      })
+      .catch(err => {
+        console.error(err);
+        if (onResolve) onResolve();
+      });
+    onCancel();
+    this.setState({ projectID: '' });
+  };
+
+  selectProject = e => {
+    this.setState({ projectID: e.target.value });
+  };
+
+  renderUploadFileButton = () => {
+    return (
+      <div className="upload-file">
+        <span className="tiffForm-label__select">Select file: </span>
+        <input
+          type="file"
+          className="upload-display"
+          multiple={true}
+          // name="tiff"
+          onChange={this.onSelectFile}
+        />
+      </div>
+    );
+  };
+
+  render = () => {
+    let disabled = this.state.files.length === 0;
+    let className = 'alert-upload';
+    className = this.props.className
+      ? `${className} ${this.props.className}`
+      : className;
+    const { projects } = this.state;
+    return (
+      <Modal.Dialog id="modal-fix">
+        <Modal.Header className="modal-header">
+          <Modal.Title>Upload CSV</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="notification-modal">
+          {this.renderUploadFileButton()}
+        </Modal.Body>
+        <Modal.Footer className="modal-footer__buttons">
+          <Button variant="secondary" onClick={this.onUpload} disabled={disabled}>
+            Submit
+          </Button>
+          <Button variant="secondary" onClick={this.props.onCancel}>Cancel</Button>
+        </Modal.Footer>
+      </Modal.Dialog>
+    );
+  };
+}
+
+UploadCSV.propTypes = {
+  onCancel: PropTypes.func.isRequired,
+  clearTreeData: PropTypes.func,
+  onResolve: PropTypes.func,
+  onSubmit: PropTypes.func,
+  pid: PropTypes.string,
+  selectedPatients: PropTypes.object,
+  selectedSeries: PropTypes.object,
+  selectedStudies: PropTypes.object,
+  clearTreeExpand: PropTypes.func
+
+};
+
+const mapStateToProps = state => {
+  return {
+    selectedPatients: state.annotationsListReducer.selectedPatients,
+    selectedStudies: state.annotationsListReducer.selectedStudies,
+    selectedSeries: state.annotationsListReducer.selectedSeries
+  };
+};
+
+export default connect(mapStateToProps)(UploadCSV);
