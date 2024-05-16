c4e00f7b39904cfc555be71e98ebf1d17a48eba3
OzgeYurtsever <ozge.ikiz.yurtsever@gmail.com>
2024-05-16 10:08:24 -0700

    Implement upload json file

--- src/components/management/upload/CsvModal.jsx
+++ src/components/management/upload/CsvModal.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { toast } from 'react-toastify';
 import { uploadBulk } from "services/annotationServices";
+import { uploadFileToProject } from '../../../services/projectServices';
 import "./csvModal.css";
 
 const CsvModal = (props) => {
@@ -13,7 +14,18 @@ const CsvModal = (props) => {
         setFiles(selectedFiles);
       };
 
-    const handleUpload = () => {
+    const handleUpload = (e) => {
+        if (files.length === 0) {
+            toast.error("Please select files before uploading!", {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+            });
+            return;
+        }
         const promises = [];
         const formData = new FormData();
         files.forEach((file, index) => {
@@ -24,9 +36,11 @@ const CsvModal = (props) => {
             'content-type': 'multipart/form-data'
           }
         };
-        promises.push(uploadBulk(formData, config))
+        if (uploadSpreadsheet) promises.push(uploadBulk(formData, config));
+        else promises.push(uploadFileToProject(formData, config, props.pid));
+
         Promise.all(promises).then((res) => {
-            toast.success("Aim upload is successful!", {
+            toast.success("File's been sent for saving!", {
                 position: "top-right",
                 autoClose: 5000,
                 hideProgressBar: false,
@@ -34,8 +48,10 @@ const CsvModal = (props) => {
                 pauseOnHover: true,
                 draggable: true,
               });
+            props.onCancel();
         }).catch(err => {
             console.error(err);
+            props.onCancel();
             toast.error("Could not upload the aim(s)!", {
                 position: "top-right",
                 autoClose: 5000,
@@ -45,10 +61,8 @@ const CsvModal = (props) => {
                 draggable: true,
               });
         });
-        props.onCancel();
     }
 
-    console.log(" ----> here")
     return (
         <div className="csv-modal-body">   
             <div className="overlay-dk">
@@ -63,14 +77,13 @@ const CsvModal = (props) => {
                             <a dataBs-toggle="collapse" href="#collapseDescription" role="button" aria-expanded="false"
                                 aria-controls="collapseDescription">
                             <input className="form-radio-input" type="radio" value="" id="flexCheckDefault" onChange={() => {setUploadSpreadsheet(true); setUploadAimFile(false)}} checked={uploadSpreadsheet} />
-                                </a> 
-                                I have a spreadsheet of cases and I want to batch create AIM files.
+                                </a>  I have a spreadsheet of cases and I want to batch create AIM files.
                             {uploadSpreadsheet && <div className="top-margin" id="collapseDescription">
                                 <p className="indent">Your file has to be in comma separated value (csv) format and contain the critical
                                 information and identical header titles needed to create a valid teaching file. <br />A sample csv file can
                                 be downloaded <a href="#">here</a>.</p>
                                 <div className="container">
-                                <label htmlFor="formFile" className="form-label">Upload CSV file to bootstrap teaching files in bulk:</label>
+                                <label htmlFor="formFile" id="upload-info" className="form-label">Upload CSV file to bootstrap teaching files in bulk:</label>
                                 <input className="form-control" type="file" id="formFile" onChange={onSelectFile}/>
                             </div>
                             </div>}
@@ -81,7 +94,7 @@ const CsvModal = (props) => {
                             <input className="form-radio-input" type="radio" value="" id="flexCheckDefault" onChange={() => { setUploadAimFile(true); setUploadSpreadsheet(false)}} checked={uploadAimFile} /></a> I have or just created AIM
                             files and want to upload them to STELLA!.
                         {uploadAimFile && <div className="container top-margin" id="collapseUpload">
-                            <label htmlFor="formFile" className="form-label">Upload JSON files:</label>
+                            <label htmlFor="formFile" id="upload-info" className="form-label">Upload JSON files:</label>
                             <input className="form-control" type="file" id="formFileMultiple" onChange={onSelectFile} multiple />
                         </div>}
                         </div>
@@ -90,11 +103,6 @@ const CsvModal = (props) => {
                         <button className="btn btn-sm btn-secondary btn-selection" onClick={props.onCancel}> Cancel </button>
                         </div>
                     </div>
-                
-
-
-
-
                 </div>
             </div>
         </div>
