16948eca115b178bcc608a2654e9109c29c5c5c1
OzgeYurtsever <ozge.ikiz.yurtsever@gmail.com>
2024-08-25 13:27:36 -0700

    Revert "Feat/metadata"

--- src/components/display/displayView.jsx
+++ src/components/display/displayView.jsx
@@ -1107,7 +1107,6 @@ class DisplayView extends Component {
 
     if (imageUrls[firstSeriesIndex] && imageUrls[firstSeriesIndex][0]) {
       if (!useSeriesData) {
-        console.log(" ----> imagedata", wadoUrlNoWadors + imageUrls[firstSeriesIndex][0].lossyImage);
         const result = await getImageMetadata(
           wadoUrlNoWadors + imageUrls[firstSeriesIndex][0].lossyImage
         );
@@ -1161,7 +1160,6 @@ class DisplayView extends Component {
       let distance = null;
       try {
         if (!useSeriesData) {
-          console.log(" ---> baseUrl", baseUrl);
           const result = await getImageMetadata(baseUrl);
           const data = result.data;
           imgData = data[0];
@@ -2588,7 +2586,6 @@ class DisplayView extends Component {
     // if (this.state.data[0])
     // console.log(this.state.data[0].stack.imageIds.length);
     // if (this.state.redirect) return <Redirect to="/list" />;
-    const stack = data[activePort] ? data[activePort].stack : null;
     const redirect = mode === "teaching" ? "search" : "list";
     let invertMap = sessionStorage.getItem("invertMap");
     invertMap = invertMap ? JSON.parse(invertMap) : {};
@@ -2611,7 +2608,6 @@ class DisplayView extends Component {
           <ToolMenu
             onSwitchView={this.props.onSwitchView}
             onInvertClick={this.formInvertMap}
-            imageData={stack && stack.imageIds ? {'imageID': stack.imageIds[stack.currentImageIdIndex], 'index': stack.currentImageIdIndex }: null}
           />
           {this.state.isLoading && (
             <div style={{ marginTop: "30%", marginLeft: "50%" }}>
