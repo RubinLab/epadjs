b6bf560b36d1dc0797238e88c88e1df99866027d
OzgeYurtsever <ozge.ikiz.yurtsever@gmail.com>
2024-11-11 15:05:42 -0800

    Force getSeries when coming from Sectra on App level

--- src/App.js
+++ src/App.js
@@ -818,7 +818,7 @@ class App extends Component {
       if (TF.total_rows) {
         this.displaySeries(packedData);
       } else {
-        const seriesArray = await this.getSeriesData(packedData);
+        const seriesArray = await this.getSeriesData(packedData, '--> handleArgs');
         window.dispatchEvent(
           new CustomEvent("openTeachingFilesModal", {
             detail: { seriesArray, args, packedData },
@@ -836,7 +836,7 @@ class App extends Component {
   };
 
   displaySeries = async (studyData) => {
-    const rawSeriesArray = await this.getSeriesData(studyData);
+    const rawSeriesArray = await this.getSeriesData(studyData, '--> displayseries');
     if (!rawSeriesArray) return;
     let seriesArr = rawSeriesArray.filter(isSupportedModality);
     let significantSeries = seriesArr.filter(
@@ -878,7 +878,8 @@ class App extends Component {
     return true;
   };
 
-  getSeriesData = async (studyData) => {
+  getSeriesData = async (studyData, fm) => {
+    console.log(" data app js fm", fm);
     const { projectID, patientID, studyUID } = studyData;
     const { seriesData } = this.props;
     try {
@@ -887,7 +888,7 @@ class App extends Component {
         seriesData[projectID][patientID][studyUID] &&
         seriesData[projectID][patientID][studyUID].list;
       if (!dataExists) {
-        const { data: series } = await getSeries(projectID, patientID, studyUID, false, "App.js, getSeriesData");
+        const { data: series } = await getSeries(projectID, patientID, studyUID, true, "App.js, getSeriesData");
         this.props.dispatch(setSeriesData(projectID, patientID, studyUID, series, true));
         this.setState({ teachingLoading: false });
         return series;
