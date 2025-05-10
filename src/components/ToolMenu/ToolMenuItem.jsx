62278f1ff0354c81fd1fc8749bb2534bb15500d4
OzgeYurtsever <ozge.ikiz.yurtsever@gmail.com>
2025-05-09 22:55:34 -0700

    Implement new tooltips

--- src/components/ToolMenu/ToolMenuItem.jsx
+++ src/components/ToolMenu/ToolMenuItem.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import ReactTooltip from "react-tooltip";
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Tooltip from 'react-bootstrap/Tooltip';
 import { reverseKeyMap } from '../../constants';
 
 class ToolMenuItem extends Component {
@@ -7,35 +8,50 @@ class ToolMenuItem extends Component {
     this.props.onClick();
   };
 
+  renderTooltip = (props) => {
+    const { name } = this.props;
+    const hotkey = reverseKeyMap[name];
+    if (hotkey) {
+      return (
+        <Tooltip id="button-tooltip" {...props}>
+          {`hotkey: ${hotkey}`}
+        </Tooltip>
+      );
+    }
+    return null;
+  }
+
   render() {
     const { index, isActive, icon, name, children } = this.props;
-    return (
-      <>
-        <div
-          tabIndex={index}
-          className={
-            isActive ? "toolbarSectionButton_Active" : "toolbarSectionButton"
-          }
-          onClick={this.handleClick}
-          data-for={`${name}-icon`}
-          >
-          <div className="toolContainer">{icon}</div>
-          <div className="buttonLabel">
-            <span>{name}</span>
-          </div>
-          {children}
+    const hotkey = reverseKeyMap[name];
+
+    const menuItem = (
+      <div
+        tabIndex={index}
+        className={isActive ? "toolbarSectionButton_Active" : "toolbarSectionButton"}
+        onClick={this.handleClick}
+      >
+        <div className="toolContainer">{icon}</div>
+        <div className="buttonLabel">
+          <span>{name}</span>
         </div>
-        {reverseKeyMap[name] && <ReactTooltip
-          id={`${name}-icon`}
-          place="top"
-          type="info"
-          delayShow={500}
-        >
-          <span className="filter-label">{`hotkey: ${reverseKeyMap[name]}`}</span>
-        </ReactTooltip>
-        }
-      </>
+        {children}
+      </div>
     );
+
+    if (hotkey) {
+      return (
+        <OverlayTrigger
+          placement="right"
+          delay={{ show: 500, hide: 250 }}
+          overlay={this.renderTooltip}
+        >
+          {menuItem}
+        </OverlayTrigger>
+      );
+    }
+
+    return menuItem;
   }
 }
 
