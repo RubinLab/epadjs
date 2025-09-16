77861a7efd25286dc6a824652e6e95790ea47735
OzgeYurtsever <ozge.ikiz.yurtsever@gmail.com>
2025-09-16 02:01:07 -0700

    Implement show-hide logic in Displayview

--- src/react-cornerstone-viewport/CornerstoneViewport/CornerstoneViewport.js
+++ src/react-cornerstone-viewport/CornerstoneViewport/CornerstoneViewport.js
@@ -328,6 +328,7 @@ class CornerstoneViewport extends Component {
    */
   getOverlay() {
     const { viewportOverlayComponent: Component, imageIds } = this.props;
+
     const {
       imageIdIndex,
       scale,
@@ -339,7 +340,7 @@ class CornerstoneViewport extends Component {
     return (
       imageId &&
       windowWidth &&
-      isOverlayVisible && (
+      this.props.showingPHI && isOverlayVisible && (
         <Component
           imageIndex={imageIdIndex + 1}
           stackSize={imageIds.length}
@@ -589,7 +590,7 @@ class CornerstoneViewport extends Component {
   onImageRendered = (event) => {
     const { viewport, element, image } = event.detail;
     const { viewportIndex } = this.props;
-    
+
     // let wwwc = sessionStorage.getItem('wwwc');
     let imgStatus = sessionStorage.getItem('imgStatus');
     imgStatus = JSON.parse(imgStatus);
@@ -623,7 +624,7 @@ class CornerstoneViewport extends Component {
       viewport.scale = zoom;
     }
 
-    if (event.detail.enabledElement.layers.length===0) {
+    if (event.detail.enabledElement.layers.length === 0) {
       viewport.voi.windowCenter = wc;
       viewport.voi.windowWidth = ww;
 
